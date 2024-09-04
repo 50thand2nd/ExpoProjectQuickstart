a7d3a75950d115fed6d462d935fe7ed45af2373c
Eash Aggarwal <eash268@gmail.com>
2024-09-04 12:45:22 -0400

    save

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text, Image, Button } from "react-native";
 import { useAuth } from "@clerk/clerk-expo";
 import { useUser } from "@clerk/clerk-expo";
+import Toast from "react-native-toast-message";
+import { BACKEND_URL } from "../constants/constants";
 
 export const SignOut = () => {
   const { isLoaded, signOut } = useAuth();
@@ -10,6 +12,31 @@ export const SignOut = () => {
   if (!isLoaded || !userIsLoaded || !isSignedIn || !user) {
     return null;
   }
+
+  const deleteAccount = async () => {
+    try {
+      // Make a request to your server to delete the user account
+      await fetch(
+        `${BACKEND_URL}/example_delete/${user.primaryEmailAddress?.emailAddress}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${user.publicMetadata?.token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await user.delete();
+      signOut();
+      Toast.show({
+        type: "success",
+        text1: "Account Deleted",
+        text2: "Your account has been successfully deleted.",
+      });
+    }
+  };
 
   return (
     <View>
@@ -21,6 +48,12 @@ export const SignOut = () => {
       <Text>{user.fullName}</Text>
       <Text>{user.primaryEmailAddress?.emailAddress}</Text>
       <Text>{user.lastSignInAt?.toString()}</Text>
+      <Button
+        title="Delete Account"
+        onPress={() => {
+          deleteAccount();
+        }}
+      />
       <Button
         title="Sign Out"
         onPress={() => {
